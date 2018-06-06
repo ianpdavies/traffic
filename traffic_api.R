7a1067e9e522f63b6fc460e938d35412ca8cbbdb
Mathis Loïc Messager <messamat@uw.edu>
2018-06-05 19:17:57 -0700

    - Correct a looping glitch for images > 2 rows

--- traffic_api.R
+++ traffic_api.R
@@ -31,9 +31,7 @@ library(RStoolbox) # don't need this - perhaps for sclass?
 #==================================================================
 
 source("map_api_edit.R") # edited function GetBingMaps from package `RGoogleMaps`
-
 load("mapValues")
-
 
 #==================================================================
 # Download static images
@@ -53,13 +51,15 @@ imgs <- c() # holds images
 coords <- NULL # holds coordinates of each image
 for(i in 1:imgs.h){ # loops over rows
   extent.img <- extent.img1 # reset back to first image and navigate down to the ith row
-  di <- ifelse(i < 2, 0, img.size*2) # only starts moving after the first image is saved
+  di <- ifelse(i < 2, 0, img.size*((i-1)*2)) # only starts moving after the first image is saved
   extent.img <- c(destPointRhumb(c(extent.img[2],extent.img[1]), 180, d=di, r=radius)[2:1], # move south one image to new row
                   destPointRhumb(c(extent.img[4],extent.img[3]), 180, d=di, r=radius)[2:1])
   for(j in 1:imgs.w){ # loops over columns
     dj <- ifelse(j < 2, 0, img.size*2)
     extent.img <- c(destPointRhumb(c(extent.img[2],extent.img[1]), 90, d=dj, r=radius)[2:1], # move east one image to new column
                     destPointRhumb(c(extent.img[4],extent.img[3]), 90, d=dj, r=radius)[2:1])
+    print(paste0('j:',j,'-',extent.img))
+
     filename <- paste(time.stamp, # time stamp
                       str_pad(j, nchar(imgs.h), pad = "0"), "_", # pad img number with leading zeros and row number
                       str_pad(i, nchar(imgs.w), pad = "0"), # pad img number with leading zeros and column number
@@ -84,6 +84,8 @@ mu<-list(image_read(paste(getwd(), "/", imgs, sep=""))) # get all saved images f
 
 p<-list() # empty list to hold appended rows of images
 for(i in 1:imgs.h){ # for each row of images, create an element in list p of east-west appended images
+  print(i)
+  print((imgs.w*(i-1)+1):(imgs.w*i))
   p[[i]] <- image_append(mu[[1]][(imgs.w*(i-1)+1):(imgs.w*i)])
 }
 
@@ -96,7 +98,7 @@ if(imgs.h > 1){ # if there's more than one row of images, append the other rows
 rm(i,j)
 
 # save mosaic raster
-image_write(w, path=paste(time.stamp, "mosaic", sep=""), format="png")
+image_write(w, path=paste(time.stamp, "mosaic.png", sep=""), format="png")
 
 # create log of mosaic image names
 write(paste(time.stamp, "mosaic.png", sep=""), file="image_log.txt", append=TRUE)
@@ -108,7 +110,7 @@ rm(mu)
 #==================================================================
 # Georeference the raster
 #==================================================================
-r<-brick(paste(time.stamp, "mosaic", sep=""), package="raster") # convert mosaic image to a rasterbrick object
+r<-brick(paste(time.stamp, "mosaic.png", sep=""), package="raster") # convert mosaic image to a rasterbrick object
 crs(r) <- myProj # add coordinate system
 
 ## georeference corners
@@ -126,8 +128,7 @@ names(r) = c("band1","band2","band3") # give image bands the same names as those
 rclass <- predict(r, sclass$model) # classify using model generated from training points
 
 # save as compressed geotiff
-writeRaster(rclass, filename=paste(time.stamp, "class.tif", sep=""), format="GTiff", 
-            options=c("COMPRESS=JPEG"), overwrite=TRUE)
+writeRaster(rclass, filename=paste(time.stamp, "class.tif", sep=""), format="GTiff", overwrite=TRUE)
 
 # create log of classified image names
 write(paste(time.stamp, "class.tif", sep=""), file="classified_image_log.txt", append=TRUE)
@@ -204,126 +205,6 @@ plot(rclass)
 #===================================================================
 #===================================================================
 
-library(RgoogleMaps) # for accessing Bing Maps API
-library(stringr) # stringr::str_pad for file naming
-library(geosphere) # for spherical geometry calculations
-library(raster) # for classification and extraction to road network
-library(rgdal) # for geographic transformations and projections
-library(magick) # for mosaicking static images together
-library(RStoolbox) # don't need this
-#>>>>>>> c9877345f9199d30ccc828885f39e7b27f749288
-
-
-#==================================================================
-# Downloading static maps 
-#==================================================================
-
-#setwd("C:/Mathis/ICSL/stormwater/src")
-source("map_api_edit.R")
-
-apiKey = "AinLOS3zG8oO80pPTZqNx_Pl4SQvO-JhY6tNCujUOJr0iRrbACjQSuLE3_9ir849"
-setwd(paste(getwd(), "/images", sep=""))
-
-# prior full extent from user
-bbox <- c(47.614086, -122.235819, 47.637687, -122.128015) # lower left , upper right lat/long
-# extents = matrix(bbox[c(3,2)]) # getting all 4 corners, for georectifying later 
-
-# calculate optimal number of images
-zoom <- 15
-radius = 6378137 # radius of earth used in Bing Maps
-px = 640 # number of pixels in image
-groundres <- (cos(bbox[1] * pi/180) * 2 * pi * radius) / (256 * 2^zoom) # size of pixel in meters for zoom level at latitude
-img.size <- px*groundres # number of pixels times ground res = image height/width in meters
-bbox.w <- distRhumb(bbox[c(2,3)], bbox[c(4,3)], r=radius) # ground distance of bounding box (east to west), top left corner to top right
-bbox.h <- distRhumb(bbox[c(2,3)], bbox[c(2,1)], r=radius) # ground distance of bounding box (north to south), top left corner to bottom left corner
-imgs.w <- ceiling(bbox.w/img.size) # optimal number of images east to west
-imgs.h <- ceiling(bbox.h/img.size) # optimal number of images north to south
-
-# loop through images
-map.params <- list(maptype="Aerial", # use aerial because it doesn't have labels
-                   zoom = zoom,
-                   apiKey=apiKey,
-                   extraURL="&mapLayer=TrafficFlow",
-                   verbose=1)
-
-# loops through and retrieves spetatic images
-time.stamp <- format(Sys.time(), "%a%d%b%y_%H_%M_") # want all images taken in a set to have same timestamp
-extent.img1 <- c(destPointRhumb(c(bbox[2],bbox[3]), 180, d=img.size, r=radius)[2:1], # first img starts near the northwestern corner (but need to do math to get the lower left and upper right coordinates of that img, which may be smaller than coords of initial bounding box)
-                 destPointRhumb(c(bbox[2],bbox[3]), 90, d=img.size, r=radius)[2:1]) # upper right corner of first img
-
-if(!exists("temp")) { # if temp file already exists, don't retrieve duplicate static images. When we parameterize the function, remove this
-  z <- 0 # what is this for? might not need it anymore
-  imgs <- list() # holds images
-  coords <- NULL # holds coordinates ... might not need it later
-  for(i in 1:imgs.h){ # loops over rows
-    extent.img <- extent.img1 # reset back to first image and navigate down to the ith row
-    di <- ifelse(i < 2, 0, img.size*2) # only starts moving after the first image
-    extent.img <- c(destPointRhumb(c(extent.img[2],extent.img[1]), 180, d=di, r=radius)[2:1], # move south one image to new row
-                    destPointRhumb(c(extent.img[4],extent.img[3]), 180, d=di, r=radius)[2:1])
-    for(j in 1:imgs.w){ # loops over columns
-      dj <- ifelse(j < 2, 0, img.size*2)
-      extent.img <- c(destPointRhumb(c(extent.img[2],extent.img[1]), 90, d=dj, r=radius)[2:1], # move east one image to new column
-                      destPointRhumb(c(extent.img[4],extent.img[3]), 90, d=dj, r=radius)[2:1])
-      filename <- paste(time.stamp, # time stamp
-                        str_pad(j, nchar(imgs.h), pad = "0"), "_", # pad img number with leading zeros, row number
-                        str_pad(i, nchar(imgs.w), pad = "0"), # pad img number with leading zeros, column number
-                        ".png", sep="")
-      map <- do.call(GetBingMap2, c(list(mapArea=extent.img, 
-                                         destfile=filename), map.params))
-      imgs <- c(imgs, filename) # list of filenames
-      coords.tmp <- c(XY2LatLon(map, -px, px, zoom), # lat/long of corners
-                      XY2LatLon(map, -px, -px, zoom), 
-                      XY2LatLon(map, px, -px, zoom),
-                      XY2LatLon(map, px, px, zoom))
-      coords <- rbind(coords, coords.tmp) # upper left, lower left, lower right, upper right
-    }
-  }
-  rm(i,j,z,coords.tmp, map, extent.img1) # remove temp objects
-  
-  save(imgs, coords, file="temp") # save for testing, not needed for final function
-} else {
-  
-  load("temp")
-}
-
-
-#==================================================================
-# mosaic images into one raster
-#==================================================================
-
-q<-list(image_read(paste(getwd(), "/", imgs, sep=""))) # get all images from files
-
-p<-list() # empty list to hold appended rows of images
-for(i in 1:imgs.h){ # for each row of images, create an element in list p of east-west appended images
-  p[[i]] <- image_append(q[[1]][(imgs.w*(i-1)+1):(imgs.w*i)])
-}
-
-w <- p[[1]] # create mosaicked image starting with first row 
-if(imgs.h > 1){ # if there's more than one row of images, append the other rows
-  for(j in 2:imgs.h){
-    w <- image_append(c(w, p[[j]]), stack=TRUE)
-  }
-}
-rm(i,j)
-
-# save mosaic raster
-image_write(w, path="test_append.png", format="png")
-
-#==================================================================
-# georeference the raster
-#==================================================================
-
-# get raster
-r<-brick("test_append.png")
-
-# add coordinate system
-crs(r) <- myProj
-
-# georeference corners
-xmax(r) <- coords[imgs.w, 8] # max lon
-xmin(r) <- coords[1,2] # min lon
-ymax(r) <- coords[1,1] # max lat
-ymin(r) <- coords[(((imgs.h-1) * imgs.w) + 1), 3] # min lat
 
 #==================================================================
 # TESTING DIFFERENT METHODS
@@ -331,66 +212,64 @@ ymin(r) <- coords[(((imgs.h-1) * imgs.w) + 1), 3] # min lat
 
 #===================================
 # get test images
+apiKey = "AinLOS3zG8oO80pPTZqNx_Pl4SQvO-JhY6tNCujUOJr0iRrbACjQSuLE3_9ir849"
 
-# smaller image to test, 1
-map.test=GetBingMap2( # aerial image
-    mapArea=c(47.591640, -122.324618, 47.598470, -122.315305),
-    maptype="CanvasDark", # use aerial for final because it doesn't have labels
-    # zoom=15,
-    apiKey=apiKey,
-    extraURL="&mapLayer=TrafficFlow",
-    destfile="test.png",
-    verbose=1,
-    labels=FALSE
-  )
-
-image_write(image_read("test.png"), path="test.png", format="png") # not sure why we have to do this. otherwise, just using brick("test.png") results in 1 band rasterbrick
-
-# smaller image to test, 2
-map.test=GetBingMap2( # aerial image
-  mapArea=c(47.556256, -122.296838, 47.565523, -122.281560),
-  maptype="CanvasDark", # use aerial for final because it doesn't have labels
-  # zoom=15,
-  apiKey=apiKey,
-  extraURL="&mapLayer=TrafficFlow",
-  destfile="test2.png",
-  verbose=1,
-  labels=FALSE
-)
-
-image_write(image_read("test2.png"), path="test2.png", format="png") # not sure why we have to do this. otherwise, just using brick("test.png") results in 1 band rasterbrick
-
-# smaller image to test, 3, but this time at the minimum map tile size
-
-z4 <- c(-122.324618, 47.598470) # top left corner
-z1<-destPointRhumb(z4, b=90, r=radius, d=img.size) # find distance to top right corner using the size of map tile at zoom 15, 640 px
-z2 <- destPointRhumb(z1, b=180, r=radius, d=img.size)# bottom right corner
-z3 <- destPointRhumb(z2, b=-90, r=radius, d=img.size)# bottom left corner
-
-
-map.test=GetBingMap2(
-  mapArea=c(z3[2], z3[1], z1[2], z1[1]),
-  maptype="CanvasDark",
-  # zoom=15,
-  apiKey=apiKey,
-  extraURL="&mapLayer=TrafficFlow",
-  destfile="test3.png",
-  verbose=1,
-  labels=FALSE
-)
-
-image_write(image_read("test3.png"), path="test3.png", format="png")
-
-# read png as rasterbricks
-r<-brick("test.png")
-r2<-brick("test2.png")
-r3<-brick("test3.png")
-crs(r) <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"
-crs(r2) <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"
-crs(r3) <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"
-
-save(r,r2, r3, file="test_imgs")
-load("test_imgs")
+# # smaller image to test, 1
+# map.test=GetBingMap2( # aerial image
+#     mapArea=c(47.591640, -122.324618, 47.598470, -122.315305),
+#     maptype="Aerial", # use aerial for final because it doesn't have labels
+#     # zoom=15,
+#     apiKey=apiKey,
+#     extraURL="&mapLayer=TrafficFlow",
+#     destfile="test.png",
+#     verbose=1
+#   )
+# #,labels=FALSE
+# image_write(image_read("test.png"), path="test.png", format="png") # not sure why we have to do this. otherwise, just using brick("test.png") results in 1 band rasterbrick
+# 
+# # smaller image to test, 2
+# map.test=GetBingMap2( # aerial image
+#   mapArea=c(47.556256, -122.296838, 47.565523, -122.281560),
+#   maptype="Aerial", # use aerial for final because it doesn't have labels
+#   # zoom=15,
+#   apiKey=apiKey,
+#   extraURL="&mapLayer=TrafficFlow",
+#   destfile="test2.png",
+#   verbose=1
+# )
+# #,labels=FALSE
+# image_write(image_read("test2.png"), path="test2.png", format="png") # not sure why we have to do this. otherwise, just using brick("test.png") results in 1 band rasterbrick
+# 
+# # smaller image to test, 3, but this time at the minimum map tile size
+# 
+# z4 <- c(-122.324618, 47.598470) # top left corner
+# z1<-destPointRhumb(z4, b=90, r=radius, d=img.size) # find distance to top right corner using the size of map tile at zoom 15, 640 px
+# z2 <- destPointRhumb(z1, b=180, r=radius, d=img.size)# bottom right corner
+# z3 <- destPointRhumb(z2, b=-90, r=radius, d=img.size)# bottom left corner
+# 
+# 
+# map.test=GetBingMap2(
+#   mapArea=c(z3[2], z3[1], z1[2], z1[1]),
+#   maptype="Aerial",
+#   # zoom=15,
+#   apiKey=apiKey,
+#   extraURL="&mapLayer=TrafficFlow",
+#   destfile="test3.png",
+#   verbose=1
+# )
+# #,labels=FALSE
+# image_write(image_read("test3.png"), path="test3.png", format="png")
+# 
+# # read png as rasterbricks
+# r<-brick("test.png")
+# r2<-brick("test2.png")
+# r3<-brick("test3.png")
+# crs(r) <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"
+# crs(r2) <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"
+# crs(r3) <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"
+# 
+# save(r,r2, r3, file="test_imgs")
+# load("test_imgs")
 
 
 #==============================================
@@ -401,14 +280,15 @@ load("test_imgs")
 # pts<-click(r,n=21,id=TRUE,xy=TRUE,cell=TRUE)
 # pts$class=c("G","Y","O","R","B","A","W") # add text labels
 # 
-# pts2<-click(r,n=21,id=TRUE,xy=TRUE,cell=TRUE)
-# pts2$class=c("G","G","G",
-#              "Y","Y","Y",
-#              "O","O","O",
-#              "R","R","R",
-#              "B","B","B",
-#              "A","A","A",
-#              "W","W","W") # add text labels
+plotRGB(r)
+ptsg<-click(r,n=42,id=TRUE,xy=TRUE,cell=TRUE)
+pts2$class=c("G","G","G","G","G","G",
+             "Y","Y","Y","Y","Y","Y",
+             "O","O","O","O","O","O",
+             "R","R","R","R","R","R",
+             "B","B","B","B","B","B",
+             "A","A","A","A","A","A",
+             "W","W","W","W","W","W") # add text labels
 
 # load("training_pts")
 load("training_pts")
@@ -437,7 +317,7 @@ sclass2 <- predict(r2, sclass$model) # classify using model generated from train
 names(r3) <- names(r) # give dataframe columns (i.e. image bands) the same names
 sclass3 <- predict(r3, sclass$model) # classify using model generated from training points
 
-# save(sclass, sclass2, sclass3, file="sclasses")
-# load("sclasses")
+save(sclass, sclass2, sclass3, file="sclasses")
+load("sclasses")
 
 

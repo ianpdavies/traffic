6c42038c366d50f1924de5ed5708bbd98002f73f
Mathis Loïc Messager <messamat@uw.edu>
2018-06-14 09:23:30 -0700

    - Begin optimizing/benchmarking function

--- run_script.R
+++ run_script.R
@@ -15,8 +15,8 @@ library(geosphere)
 library(tictoc)
 
 source('coord_conversion.R')
-
-datadir <- 'F:/Levin_Lab/stormwater/data/'
+setwd('F:/Levin_Lab/stormwater/src/traffic')
+datadir <- 'F:/Levin_Lab/stormwater/results/'
 #==================================================================
 # Set constants and map parameters
 #==================================================================
@@ -26,16 +26,16 @@ datadir <- 'F:/Levin_Lab/stormwater/data/'
 apiKey = "AinLOS3zG8oO80pPTZqNx_Pl4SQvO-JhY6tNCujUOJr0iRrbACjQSuLE3_9ir849"
 
 # Full extent of area to be covered
-PSwatershed <- readOGR(file.path(datadir, 'NOAA_ERMA_20180601/PugetSoundWtshd/PS_watersheds.shp'))
+PSwatershed <- readOGR(file.path(datadir, 'PSwtshd_dissolve.shp'))
 PSwatershedbbox <- spTransform(PSwatershed, CRSobj=CRS("+proj=longlat +datum=WGS84"))@bbox
+polybound <- TRUE
 
-
-#bbox <- c(47.5, -122.5, 48, -122)  # Coordinates are lower left and upper right lat/long (in that order)
-bbox <- c(PSwatershedbbox[2,1],PSwatershedbbox[1,1],PSwatershedbbox[2,2],PSwatershedbbox[1,2])  # Coordinates are lower left and upper right lat/long (in that order)
+bbox <- c(46.7, -123, 47.2, -122.5)  # Coordinates are lower left and upper right lat/long (in that order)
+#bbox <- c(PSwatershedbbox[2,1],PSwatershedbbox[1,1],PSwatershedbbox[2,2],PSwatershedbbox[1,2])  # Coordinates are lower left and upper right lat/long (in that order)
 
 # calculate optimal number of images to fetch
 zoom <- 15
-px = 500 # length of the image in pixels (maximum that Bing/Google will output)
+px = 1500 # length of the image in pixels (maximum that Bing/Google will output)
 
 #Computer pixel coordinate X and Y for the lower left and upper right corner of every row and column
 bbox.list.ll.x <- seq(latlong_to_pixelcoords(bbox[1],bbox[2],zoom)[1],latlong_to_pixelcoords(bbox[3],bbox[4],zoom)[1],px)
@@ -46,16 +46,20 @@ bbox.list.ll.y <- bbox.list.ur.y + (px-1)
 imgs.w <- length(bbox.list.ll.x) #Number of columns
 imgs.h <- length(bbox.list.ll.y) #Number of rows
 
-save(apiKey, zoom, px, 
+save(apiKey, PSwatershed, polybound, zoom, px, 
      bbox.list.ll.x, bbox.list.ll.y, bbox.list.ur.x, bbox.list.ur.y,
      imgs.w, imgs.h, file = "mapValues")
 
 #==================================================================
 # Schedule tasks
 #==================================================================
-# tic("Download, mosaic, and georeference")
-# source("traffic_api.R")
-# toc()
+source("traffic_api.R")
+
+#For 840 tiles
+#With 500 pixel images, without selective tiling: download 296 sec, mosaic 18 min, classification and export 30 min
+#with 1500 pixel inmages, without selective tiling: download 64 sec, mosaic 5 min, classification and export 30 min
+
+
 
 # Run script hourly
 # taskscheduler_create("GetRasters", rscript="F:/Levin_Lab/stormwater/src/traffic/traffic_api.R",
